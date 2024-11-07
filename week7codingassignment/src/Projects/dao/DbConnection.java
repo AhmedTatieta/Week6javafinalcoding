b083ab8dc34f8131e2d64d1827b5764a9cc77563
AhmedTatieta <161558163+AhmedTatieta@users.noreply.github.com>
2024-11-06 17:31:56 -0800

    Pushing week7 assignment.

---
+++ week7codingassignment/src/Projects/dao/DbConnection.java
@@ -0,0 +1,15 @@
+package Projects.dao;
+
+public class DbConnection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    private static String HOST = "Localhost";
+    private static String PASSWORD = "projects";
+    private static int PORT = 3306;
+    private static String SCHEMA = "projects";
+    private static String USER = "projects";
+    
+	}
+
+}
---
+++ week7codingassignment/src/Projects/exception/DbException.java
@@ -0,0 +1,18 @@
+package Projects.exception;
+
+public class DbException {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+
+public DbException(String message) {}
+
+public DbException(Throwable cause) {}
+
+public DbException(String message, Throwable cause) {}
+
+	}
+
+}
